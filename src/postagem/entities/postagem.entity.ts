a6dfd7a243f3eb8082eabddd0b66c04c002530b6
unknown <dayse_santos22@hotmail.com>
2025-08-06 17:10:59 -0300

    Adiciona modulo auth e modulo usuario

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty } from "class-validator";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
+import { UseGuards } from "@nestjs/common";
 
 @Entity({ name: "tb_postagens" })
 export class Postagem {
@@ -22,5 +24,12 @@ export class Postagem {
     @ManyToOne(() => Tema, (tema) => tema.postagem, {
         onDelete: "CASCADE"
     })
+    @JoinColumn({name: 'tema_id'})
     tema: Tema
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: "CASCADE"
+    })
+    @JoinColumn({name: 'usuario_id'})
+    usuario: Usuario
 }
